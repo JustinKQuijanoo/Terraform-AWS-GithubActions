ef55b057a65f871f9c66199da9c6ba6b5dca9b2b
JustinKQuijanoo <quijanju@sheridancollege.ca>
2023-04-03 10:16:22 -0400

    added data for subnet id

--- load-balancer/output.tf
+++ load-balancer/output.tf
@@ -10,8 +10,16 @@ data "aws_vpc" "VPC-Hydrohomie" {
   id = var.vpc_id
 }
 
-data "aws_subnet_ids" "VPC-Hydrohomie" {
-  vpc_id = data.aws_vpc.VPC-Hydrohomie.id
+data "aws_subnets" "SN-Hydrohomie" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_subnet" "SN-Hydrohomie" {
+  for_each = toset(data.aws_subnets.example.ids)
+  id       = each.value
 }
 
 
