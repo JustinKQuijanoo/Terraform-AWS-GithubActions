b98ed33c7d6088cc4df110f540acdce5dc7166a9
JustinKQuijanoo <quijanju@sheridancollege.ca>
2023-04-13 15:47:42 -0400

    updated shared_credentials_files

--- providers.tf
+++ providers.tf
@@ -15,5 +15,5 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["/Users/justi/.aws/credentials"]
+  shared_credentials_files = ["/Users/Owner/.aws/credentials"]
 }
